59a838dae5be3b7e999dfc5e99936061c8d012fd
leonardo.dias@withclutch.com <leonardo.dias@withclutch.com>
2024-05-14 17:10:23 -0300

    adding decribe

--- src/escala/montar-escala.spec.js
+++ src/escala/montar-escala.spec.js
@@ -1,145 +1,148 @@
 import { montarEscalaDoDia } from "./montar-escala.js";
-import { expect, test } from "vitest";
-test("deve montar escala com cargo e tag", () => {
-  const locaisDeEscala = [
-    {
-      local: "Vac Gasômetro",
-      necessidades: [
-        { tag: "MOTORISTA", quantidade: 1 },
-        { cargo: "Medico", quantidade: 1 },
-      ],
-    },
-  ];
-  const profissionais = [
-    {
-      nome: "João",
-      cargo: "Medico",
-      tags: [],
-    },
-    {
-      nome: "Maria",
-      cargo: "Enfermeira",
-      tags: ["MOTORISTA"],
-    },
-    {
-      nome: "José",
-      cargo: "Enfermeira",
-      tags: [],
-    },
-  ];
-  const escalas = montarEscalaDoDia(
-    "15/05/2024",
-    locaisDeEscala,
-    profissionais
-  );
-  console.log(escalas);
-  expect(escalas).toEqual([
-    {
-      atuacao: "Medico",
-      cargo: "Medico",
-      data: "15/05/2024",
-      local: "Vac Gasômetro",
-      profissional: "João",
-      tags: [],
-    },
-    {
-      atuacao: "MOTORISTA",
-      cargo: "Enfermeira",
-      data: "15/05/2024",
-      local: "Vac Gasômetro",
-      profissional: "Maria",
-      tags: ["MOTORISTA"],
-    },
-  ]);
-});
+import { expect, test, describe } from "vitest";
 
-test("deve utilizar mesma pessoa com tag quando já esta na escala", () => {
-  const locaisDeEscala = [
-    {
-      local: "Vac Gasômetro",
-      necessidades: [
-        { tag: "MOTORISTA", quantidade: 1 },
-        { cargo: "Medico", quantidade: 1 },
-      ],
-    },
-  ];
-  const profissionais = [
-    {
-      nome: "João",
-      cargo: "Medico",
-      tags: ["MOTORISTA"],
-    },
-    {
-      nome: "Maria",
-      cargo: "Enfermeira",
-      tags: ["MOTORISTA"],
-    },
-    {
-      nome: "José",
-      cargo: "Enfermeira",
-      tags: [],
-    },
-  ];
-  const escalas = montarEscalaDoDia(
-    "15/05/2024",
-    locaisDeEscala,
-    profissionais
-  );
-  console.log(escalas);
-  expect(escalas).toEqual([
-    {
-      atuacao: "Medico",
-      cargo: "Medico",
-      data: "15/05/2024",
-      local: "Vac Gasômetro",
-      profissional: "João",
-      tags: ["MOTORISTA"],
-    },
-  ]);
-});
+describe("montarEscalaDoDia", () => {
+  test("deve montar escala com cargo e tag", () => {
+    const locaisDeEscala = [
+      {
+        local: "Vac Gasômetro",
+        necessidades: [
+          { tag: "MOTORISTA", quantidade: 1 },
+          { cargo: "Medico", quantidade: 1 },
+        ],
+      },
+    ];
+    const profissionais = [
+      {
+        nome: "João",
+        cargo: "Medico",
+        tags: [],
+      },
+      {
+        nome: "Maria",
+        cargo: "Enfermeira",
+        tags: ["MOTORISTA"],
+      },
+      {
+        nome: "José",
+        cargo: "Enfermeira",
+        tags: [],
+      },
+    ];
+    const escalas = montarEscalaDoDia(
+      "15/05/2024",
+      locaisDeEscala,
+      profissionais
+    );
+    console.log(escalas);
+    expect(escalas).toEqual([
+      {
+        atuacao: "Medico",
+        cargo: "Medico",
+        data: "15/05/2024",
+        local: "Vac Gasômetro",
+        profissional: "João",
+        tags: [],
+      },
+      {
+        atuacao: "MOTORISTA",
+        cargo: "Enfermeira",
+        data: "15/05/2024",
+        local: "Vac Gasômetro",
+        profissional: "Maria",
+        tags: ["MOTORISTA"],
+      },
+    ]);
+  });
 
-test("deve utilizar mesma pessoa com multiplas tag quando já esta na escala", () => {
-  const locaisDeEscala = [
-    {
-      local: "Vac Gasômetro",
-      necessidades: [
-        { tag: "MOTORISTA", quantidade: 1 },
-        { tag: "APOIO", quantidade: 1 },
-        { cargo: "Medico", quantidade: 1 },
-      ],
-    },
-  ];
-  const profissionais = [
-    {
-      nome: "João",
-      cargo: "Medico",
-      tags: ["MOTORISTA", "APOIO"],
-    },
-    {
-      nome: "Maria",
-      cargo: "Enfermeira",
-      tags: ["MOTORISTA", "APOIO"],
-    },
-    {
-      nome: "José",
-      cargo: "Enfermeira",
-      tags: [],
-    },
-  ];
-  const escalas = montarEscalaDoDia(
-    "15/05/2024",
-    locaisDeEscala,
-    profissionais
-  );
-  console.log(escalas);
-  expect(escalas).toEqual([
-    {
-      atuacao: "Medico",
-      cargo: "Medico",
-      data: "15/05/2024",
-      local: "Vac Gasômetro",
-      profissional: "João",
-      tags: ["MOTORISTA", "APOIO"],
-    },
-  ]);
+  test("deve utilizar mesma pessoa com tag quando já esta na escala", () => {
+    const locaisDeEscala = [
+      {
+        local: "Vac Gasômetro",
+        necessidades: [
+          { tag: "MOTORISTA", quantidade: 1 },
+          { cargo: "Medico", quantidade: 1 },
+        ],
+      },
+    ];
+    const profissionais = [
+      {
+        nome: "João",
+        cargo: "Medico",
+        tags: ["MOTORISTA"],
+      },
+      {
+        nome: "Maria",
+        cargo: "Enfermeira",
+        tags: ["MOTORISTA"],
+      },
+      {
+        nome: "José",
+        cargo: "Enfermeira",
+        tags: [],
+      },
+    ];
+    const escalas = montarEscalaDoDia(
+      "15/05/2024",
+      locaisDeEscala,
+      profissionais
+    );
+    console.log(escalas);
+    expect(escalas).toEqual([
+      {
+        atuacao: "Medico",
+        cargo: "Medico",
+        data: "15/05/2024",
+        local: "Vac Gasômetro",
+        profissional: "João",
+        tags: ["MOTORISTA"],
+      },
+    ]);
+  });
+
+  test("deve utilizar mesma pessoa com multiplas tag quando já esta na escala", () => {
+    const locaisDeEscala = [
+      {
+        local: "Vac Gasômetro",
+        necessidades: [
+          { tag: "MOTORISTA", quantidade: 1 },
+          { tag: "APOIO", quantidade: 1 },
+          { cargo: "Medico", quantidade: 1 },
+        ],
+      },
+    ];
+    const profissionais = [
+      {
+        nome: "João",
+        cargo: "Medico",
+        tags: ["MOTORISTA", "APOIO"],
+      },
+      {
+        nome: "Maria",
+        cargo: "Enfermeira",
+        tags: ["MOTORISTA", "APOIO"],
+      },
+      {
+        nome: "José",
+        cargo: "Enfermeira",
+        tags: [],
+      },
+    ];
+    const escalas = montarEscalaDoDia(
+      "15/05/2024",
+      locaisDeEscala,
+      profissionais
+    );
+    console.log(escalas);
+    expect(escalas).toEqual([
+      {
+        atuacao: "Medico",
+        cargo: "Medico",
+        data: "15/05/2024",
+        local: "Vac Gasômetro",
+        profissional: "João",
+        tags: ["MOTORISTA", "APOIO"],
+      },
+    ]);
+  });
 });
